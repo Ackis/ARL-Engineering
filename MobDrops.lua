e950e5afc1e0a94ee490150fdb6fbcbc3620e8e8
James D. Callahan III <jcallahan@curse.com>
2015-11-20 21:09:30 -0600

    Cleanups.

--- MobDrops.lua
+++ MobDrops.lua
@@ -27,8 +27,8 @@ local Z = constants.ZONE_NAMES
 function module:InitializeMobDrops()
 	local function AddMob(npcID, npcName, zoneName, coordX, coordY)
 		addon.AcquireTypes.MobDrop:AddEntity(module, {
-			coord_x = coordX,
-			coord_y = coordY,
+			coord_x = coordX or 0,
+			coord_y = coordY or 0,
 			faction = nil,
 			identifier = npcID,
 			item_list = {},
@@ -37,17 +37,17 @@ function module:InitializeMobDrops()
 		})
 	end
 
-	AddMob(7800,	BN.MEKGINEER_THERMAPLUGG,		Z.GNOMEREGAN,			0, 0)
-	AddMob(8897,	L["Doomforge Craftsman"],		Z.BLACKROCK_DEPTHS,		0, 0)
-	AddMob(8920,	L["Weapon Technician"],			Z.BLACKROCK_DEPTHS,		0, 0)
-	AddMob(9499,	BN.PLUGGER_SPAZZRING,			Z.BLACKROCK_DEPTHS,		0, 0)
-	AddMob(10264,	BN.SOLAKAR_FLAMEWREATH,			Z.BLACKROCK_SPIRE,		0, 0)
-	AddMob(10426,	L["Risen Inquisitor"],			Z.STRATHOLME,			0, 0)
-	AddMob(16152,	BB["Attumen the Huntsman"],		Z.KARAZHAN,			0, 0)
-	AddMob(17796,	BN.MEKGINEER_STEAMRIGGER,		Z.THE_STEAMVAULT,		0, 0)
-	AddMob(19219,	BN.MECHANO_LORD_CAPACITUS,		Z.THE_MECHANAR,			0, 0)
+	AddMob(7800,	BN.MEKGINEER_THERMAPLUGG,		Z.GNOMEREGAN)
+	AddMob(8897,	L["Doomforge Craftsman"],		Z.BLACKROCK_DEPTHS)
+	AddMob(8920,	L["Weapon Technician"],			Z.BLACKROCK_DEPTHS)
+	AddMob(9499,	BN.PLUGGER_SPAZZRING,			Z.BLACKROCK_DEPTHS)
+	AddMob(10264,	BN.SOLAKAR_FLAMEWREATH,			Z.BLACKROCK_SPIRE)
+	AddMob(10426,	L["Risen Inquisitor"],			Z.STRATHOLME)
+	AddMob(16152,	BB["Attumen the Huntsman"],		Z.KARAZHAN)
+	AddMob(17796,	BN.MEKGINEER_STEAMRIGGER,		Z.THE_STEAMVAULT)
+	AddMob(19219,	BN.MECHANO_LORD_CAPACITUS,		Z.THE_MECHANAR)
 	AddMob(19960,	L["Doomforge Engineer"],		Z.BLADES_EDGE_MOUNTAINS,	75.1, 39.8)
-	AddMob(20207,	L["Sunfury Bowman"],			Z.NETHERSTORM,			56.8, 64.6)
+	AddMob(20207,	L["Sunfury Bowman"],			Z.NETHERSTORM,				56.8, 64.6)
 	AddMob(23386,	L["Gan'arg Analyzer"],			Z.BLADES_EDGE_MOUNTAINS,	33.0, 52.5)
 
 	self.InitializeMobDrops = nil
